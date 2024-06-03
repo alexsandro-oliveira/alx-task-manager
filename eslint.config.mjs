1b2108b70e17f0d6d1883728f1799407df817779
Alexs <ale.osantos@hotmail.com>
2024-06-03 20:19:12 +0100

    chore: add lint-staged and husky

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,11 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import { fixupConfigRules } from "@eslint/compat";
 
 
 export default [
   {languageOptions: { globals: {...globals.browser, ...globals.node} }},
   pluginJs.configs.recommended,
-  pluginReactConfig,
+  ...fixupConfigRules(pluginReactConfig),
 ];
